7058026db7e7f414523f8cb643e5f4cf5d66e485
David Rouchy <drouchy@gmail.com>
2014-07-13 20:58:30 +0100

    configure lager & exlager

--- apps/re_inspector_app/lib/re_inspector_app.ex
+++ apps/re_inspector_app/lib/re_inspector_app.ex
@@ -1,13 +1,25 @@
 defmodule ReInspector.App do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  @lagger_formatter [:time, ' [', :severity, '] ', :message, '\n']
+  @lager_level :debug
+
   def start(_type, _args) do
+    configure_lager
     ReInspector.App.Supervisors.MainSupervisor.start_link
   end
 
   def version do
     GenServer.call(:re_inspector_config, :version)
+  end
+
+  defp configure_lager do
+    :application.set_env(:lager, :handlers, [
+      lager_console_backend: [@lager_level, { :lager_default_formatter, [:time, ' [', :severity, '] ', :message, '\n']}],
+      lager_file_backend:    [{:file, "logs/application.log"}, {:level, :debug}]
+      ], persistent: true)
+    :application.set_env(:lager, :crash_log, "logs/crash.log", persistent: true)
+
+    :application.ensure_all_started(:exlager)
   end
 end
