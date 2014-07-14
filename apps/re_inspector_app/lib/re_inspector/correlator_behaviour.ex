2eee1e84a639a4d75478f73856e5fafffd3d4f88
David Rouchy <drouchy@gmail.com>
2014-07-14 19:59:44 +0100

    define the correlator behaviour

---
+++ apps/re_inspector_app/lib/re_inspector/correlator_behaviour.ex
@@ -0,0 +1,9 @@
+defmodule ReInspector.Correlator do
+  use Behaviour
+
+  defcallback request_name(message :: Map) :: String
+  defcallback support?(message :: ReInspector.App.ApiRequest) :: Boolean
+  defcallback additional_information(message :: ReInspector.App.ApiRequest) :: Map
+  defcallback extract_correlation(message :: ReInspector.App.ApiRequest) :: List
+
+end
