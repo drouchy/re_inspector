b74ccafad6942b9a5368fe3212ef10ffa5f1e0a6
David Rouchy <drouchy@gmail.com>
2014-07-14 17:49:59 +0100

    finish the api request convertion

--- apps/re_inspector_app/lib/re_inspector/converters/api_request_message_converter.ex
+++ apps/re_inspector_app/lib/re_inspector/converters/api_request_message_converter.ex
@@ -1,12 +1,39 @@
 defmodule ReInspector.App.Processors.ApiRequestMessageConverter do
   import Lager
+  alias ReInspector.App.JsonParser
 
   def to_postgres(message) do
     Lager.debug "converting message #{inspect message}"
     message
-    |> Map.put(:service_name, message[:service][:name])
+    |> Map.put(:requested_at,    parse_date_time(message[:requested_at]))
+    |> Map.put(:duration,        message[:time_to_execute])
+    |> Map.put(:service_name,    message[:service][:name])
     |> Map.put(:service_version, message[:service][:version])
-    |> Map.put(:service_env, message[:service][:environment])
-    |> Map.drop([:service, :request, :response, :requested_at])
+    |> Map.put(:service_env,     message[:service][:environment])
+
+    |> Map.put(:request_body,    message[:request][:body])
+    |> Map.put(:request_headers, JsonParser.encode(message[:request][:headers]))
+    |> Map.put(:method,          message[:request][:method])
+    |> Map.put(:path,            message[:request][:path])
+
+    |> Map.put(:response_body,    message[:response][:body])
+    |> Map.put(:response_headers, JsonParser.encode(message[:response][:headers]))
+    |> Map.put(:status,           message[:response][:status])
+
+    |> Map.drop([:service, :request, :response])
+  end
+
+  defp parse_date_time date_string do
+    Regex.named_captures(~r/^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})Z/, date_string)
+    |> date_time_captures_to_date_time
+  end
+
+  defp date_time_captures_to_date_time captures do
+    %Ecto.DateTime{year:  String.to_integer(captures["year"]),
+                   month: String.to_integer(captures["month"]),
+                   day:   String.to_integer(captures["day"]),
+                   hour:  String.to_integer(captures["hour"]),
+                   min:   String.to_integer(captures["minute"]),
+                   sec:   String.to_integer(captures["second"])}
   end
 end
--- apps/re_inspector_app/test/re_inspector/converters/api_request_message_converter_test.exs
+++ apps/re_inspector_app/test/re_inspector/converters/api_request_message_converter_test.exs
@@ -4,11 +4,19 @@ defmodule ReInspector.App.Processors.ApiRequestMessageConverterTest do
   alias ReInspector.App.Processors.ApiRequestMessageConverter
 
   test "it converts the requested_at" do
-    assert convert[:requested_at] == "2014-01-03T14:30:00Z"
+    date = convert[:requested_at]
+
+    assert date.year == 2014
+    assert date.month == 1
+    assert date.day == 3
+
+    assert date.hour == 14
+    assert date.min == 30
+    assert date.sec == 00
   end
 
   test "it converts the time_to_execute" do
-    assert convert[:time_to_execute] == 1230
+    assert convert[:duration] == 1230
   end
 
   test "it converts the service name" do
@@ -23,6 +31,46 @@ defmodule ReInspector.App.Processors.ApiRequestMessageConverterTest do
     assert convert[:service_env] == "sandbox"
   end
 
+  test "it converts the request method" do
+    assert convert[:method] == "POST"
+  end
+
+  test "it converts the request path" do
+    assert convert[:path] == "/bookings"
+  end
+
+  test "it converts the request headers" do
+    assert convert[:request_headers] == "{\"Authorization\":\"Bearer 234......4\",\"Content-type\":\"application/json\"}"
+  end
+
+  test "it converts the request body" do
+    assert convert[:request_body] == "{ \"booking\" : { \"quote_id\": \"1a2b3c-asap\", \"booking_type\": \"general\" } }"
+  end
+
+  test "it converts the response status" do
+    assert convert[:status] == 200
+  end
+
+  test "it converts the response " do
+    assert convert[:response_headers] == "{\"content-type\":\"application/json\"}"
+  end
+
+  test "it converts the response body" do
+    assert convert[:response_body] == "{ \"booking\": { \"reference\": \"SHU31-24C43\" } }"
+  end
+
+  test "it removes the request entry" do
+    refute Map.has_key?(convert, :request)
+  end
+
+  test "it removes the response entry" do
+    refute Map.has_key?(convert, :response)
+  end
+
+  test "it removes the service entry" do
+    refute Map.has_key?(convert, :service)
+  end
+
   defp convert do
     {:ok, content} = default_fixture
     ReInspector.App.JsonParser.decode(content)
@@ -32,25 +80,3 @@ defmodule ReInspector.App.Processors.ApiRequestMessageConverterTest do
   defp default_fixture, do: File.read fixture_file
   defp fixture_file,    do: Path.expand("../../../fixtures/booking_requests/default_request.json", __ENV__.file())
 end
-
-"""
-{
-
-  "request": {
-     "method": "POST",
-     "path": "/bookings",
-     "headers": {
-       "Authorization": "Bearer 234......4",
-       "Content-type": "application/json"
-     },
-     "body": "{ \"booking\" : { \"quote_id\": \"1a2b3c-asap\", \"booking_type\": \"general\" } }"
-  },
-  "response": {
-     "status": 200,
-     "headers": {
-       "content-type": "application/json"
-     },
-     "body": "{ \"booking\": { \"reference\": \"SHU31-24C43\" } }"
-  }
-}
-"""
