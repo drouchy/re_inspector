c634a7a82350ec30da615cf65bb368ba9b59c2e0
David Rouchy <drouchy@gmail.com>
2014-08-11 12:07:57 -0700

    Fix a typo in the travis config file

--- apps/re_inspector_app/config/travis.exs
+++ apps/re_inspector_app/config/travis.exs
@@ -2,13 +2,13 @@ use Mix.Config
 
 import_config "test.exs"
 
-config :re_inspector_app, :database:
+config :re_inspector_app, :database,
   host: "localhost",
   login: "postgres",
   password: nil,
   database: "re_inspector_ci_test"
 
-config :re_inspector_app, :listeners
+config :re_inspector_app, :listeners,
   redis: [
     [
       name: "test",
