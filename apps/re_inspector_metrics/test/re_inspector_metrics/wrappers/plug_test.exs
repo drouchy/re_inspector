ecda541e25c4b1f108ff1f48c7e359eec6d64d52
David Rouchy <drouchy@gmail.com>
2014-10-27 08:39:22 +0000

    try to fix an intermittent failure

--- apps/re_inspector_metrics/test/re_inspector_metrics/wrappers/plug_test.exs
+++ apps/re_inspector_metrics/test/re_inspector_metrics/wrappers/plug_test.exs
@@ -1,6 +1,7 @@
 defmodule ReInspector.Metrics.PlugTest do
   use ExUnit.Case, async: false
   use Plug.Test
+  use Webtest.Case
   import Mock
 
   alias ReInspector.Metrics.Plug.Instrumentation
@@ -15,11 +16,13 @@ defmodule ReInspector.Metrics.PlugTest do
   # call/2
   test_with_mock "capture the elapsed time and send it to statman", ReInspector.Metrics,
   [ register_web_transaction: fn(_path, total) -> assert_in_delta(total, 50000, 10000) ; :ok end] do
-    conn = Instrumentation.call(conn(:get, "/path"), @opts)
-    :timer.sleep 50
-    conn = conn |> put_resp_content_type("text/plain")
-                |> send_resp(200, "Hello world")
+    with_retries 2, 100 do
+      conn = Instrumentation.call(conn(:get, "/path"), @opts)
+      :timer.sleep 50
+      conn = conn |> put_resp_content_type("text/plain")
+                  |> send_resp(200, "Hello world")
 
-    assert conn.status == 200
+      assert conn.status == 200
+    end
   end
 end
